f243628ba58ffdef404a4384545fcd87bbb378e5
sabrina <sabrinajaimee@gmail.com>
2020-05-28 18:53:32 -0300

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,32 @@
-@extends('plantillaadmin')
 @extends('layouts.app2')
-@section('principal')
 @section('content')
-@section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><h3>{{ __('Iniciar Sesion') }}</h3></div >
+                <div class="card-header"><h3>{{ __('Login') }}</h3></div >
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
 
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
+                                    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
-                                    <br>
+
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
-                            <div class=>
+                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -39,10 +36,9 @@
                                 @enderror
                             </div>
                         </div>
-                                <br>
 
-                        <div class="form-group ">
-                            <div class="">
+                        <div class="form-group row">
+                            <div class="col-md-6">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -51,14 +47,13 @@
                                     </label>
                                 </div>
                             </div>
-                        
-                                <br>
+                        </div>
 
-                        <div class="">
-                            <div class="">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                            <input type="submit" name="Enviar" value="Ingresar" class="bot">
+                            </div>
+                        </div>
                                     
                                     <br>
                                 @if (Route::has('password.request'))
@@ -73,12 +68,17 @@
                                     </a>
                             </div>
                         </div>
-                </div>
+
                         <br>
                     </form>
-                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+</div>            
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
 @endsection
